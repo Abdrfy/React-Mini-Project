98dee00066b23f17921145878fbef87ae55337cf
Abdul Rafay <abdrfy@gmail.com>
2023-03-27 03:41:14 +0500

    WHAT: First commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,53 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import allReducer from './reducers'
+
+const store = createStore(
+  allReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+// //ACTION
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// //REDUCER
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// }
+// let store = createStore(counter);
+
+// //DISPLAY
+// store.subscribe(() => console.log(store.getState()))
+
+// //DISPATCH
+// store.dispatch(increment())
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
